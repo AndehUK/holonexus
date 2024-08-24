e656d21c692c84a90f0fcd1c9db01760ad236b88
Andrew <andy@andeh.uk>
2024-08-24 04:04:31 +0100

    Gotta love light mode

--- src/app/(landing)/discord.tsx
+++ src/app/(landing)/discord.tsx
@@ -8,10 +8,10 @@ export const Discord = () => {
   return (
     <Card className="relative isolate overflow-hidden bg-primary/5 px-6 py-8 shadow-2xl sm:rounded-3xl sm:px-16 md:py-12 lg:flex lg:gap-x-20 lg:px-24 lg:py-0">
       <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+        <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
           Bring the Holonexus to your Discord server!
         </h2>
-        <p className="mt-6 text-lg leading-8 text-gray-300">
+        <p className="mt-6 text-lg leading-8">
           Invite our Discord bot to bring the power of the Holonexus to your
           Discord server.
         </p>
